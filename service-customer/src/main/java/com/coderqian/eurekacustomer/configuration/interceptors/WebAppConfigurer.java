8a92cc92603668c5ac967dd0be28f23d8df781e9
qianlq <1242202279@qq.com>
2018-12-14 15:52:27 +0800

    :bamboo: feat(拦截器): 添加监控拦截器

--- service-customer/src/main/java/com/coderqian/eurekacustomer/configuration/interceptors/WebAppConfigurer.java
+++ service-customer/src/main/java/com/coderqian/eurekacustomer/configuration/interceptors/WebAppConfigurer.java
@@ -7,7 +7,6 @@ import com.coderqian.eurekacustomer.interceptor.MonitorInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
